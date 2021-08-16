d4691d88d9c4c4a6118771936b9c40ea5a9132f8
lejaaprianza <lejaprianza@gmail.com>
2021-08-16 16:24:38 +0700

    part 2

---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+} from "reactstrap"
+
+const NavbarComponent = (props) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <Container>
+          <NavbarBrand href="/">Lampung Kode</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github">About Us</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default NavbarComponent
